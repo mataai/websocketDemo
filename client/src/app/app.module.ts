e128f8351cb30e2e09195e21c1923e203c8955e0
HandsomeRomanian <mat4school@gmail.com>
2020-05-04 00:15:00 -0400

    Yo

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
